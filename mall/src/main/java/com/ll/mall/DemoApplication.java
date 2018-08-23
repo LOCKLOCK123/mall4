0449daed93214b7fd517fb6b1093c90a4e68dbe7
LOCKLOCK123 <linlang1350343758@qq.com>
2018-08-23 21:50:34 +0800

    包扫描添加和mysql配置文件添加

--- mall/src/main/java/com/ll/mall/DemoApplication.java
+++ mall/src/main/java/com/ll/mall/DemoApplication.java
@@ -3,8 +3,10 @@ package com.ll.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
+@ComponentScan("controller")
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ mall/src/main/java/controller/SampleController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
