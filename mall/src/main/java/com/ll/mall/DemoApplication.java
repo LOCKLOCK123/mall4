0e395d1bda9e37aead7e1f7db75b29144f6aacb3
LOCKLOCK123 <linlang1350343758@qq.com>
2018-08-25 17:54:26 +0800

    添加注解配置包扫描

--- mall/src/main/java/com/ll/mall/DemoApplication.java
+++ mall/src/main/java/com/ll/mall/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.ll.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan("controller")
+@ComponentScan("controller.*,service.*")
+@MapperScan("com.mmall.dao")
 public class DemoApplication {
 
 	public static void main(String[] args) {
